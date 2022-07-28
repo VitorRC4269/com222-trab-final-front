756f421c91f8fe6224378ccc4ab4781c7aab596e
VitorRC4269 <vitorramosdecarvalho@gmail.com>
2022-07-28 16:21:12 -0300

    tentando arrumar

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const appName = 'Angular13Crud';
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/Angular13Crud'));
+app.use(express.static(`${__dirname}/dist/${appName}`));
 
 app.get('/*', (req, res)=> {
-    res.sendFile(__dirname + '/dist/Angular13Crud/index.html');
+    res.sendFile(path.join(`${__dirname}/dist/${appName}/index.html`));
 });
 
 app.listen(PORT);
